3a885c632abb2293b59e337e1c26fe5702adb43d
julia <bruna.lima@edu.unirio.br>
2022-10-10 09:54:20 -0300

    ATT EX 1 - 10.10

--- ex - pokemon/SCRIPT/index.js
+++ ex - pokemon/SCRIPT/index.js
@@ -1,5 +1,7 @@
 var req = new XMLHttpRequest();
 
-req.open("GET", "https://pokeapi.co/api/v2/pokemon/ditto")
+req.open("GET", "https://pokeapi.co/api/v2/pokemon-species/132/")
 
+req.send()
 
+document.write("Informações: " + req.onload(     ))
